c358fe97f80f3b3d0650b54a0277ee3bec62bb7e
BHATTACHARYA, ARUNIMA <782328@ehs-s-itc-13>
2016-01-27 11:07:21 -0600

    -Set-up Timer -Balloon Expansion

--- GameAppDesign/GameViewController.swift
+++ GameAppDesign/GameViewController.swift
@@ -10,6 +10,47 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    
+    var defaulttimer = 90
+    var timerCount = 20
+    
+    var minutes = 0
+    var seconds = 0
+    
+    
+    var timerRunning = false //tell xCode if our timer is running
+    var timer = NSTimer()
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    //code for counting down timer
+    func timerCountDown() {
+        
+        minutes = timerCount / 60
+        seconds = timerCount - (minutes * 60)
+        
+        timerCount--
+        timerLabel.text = String(format: "%02d:%02d", minutes, seconds)
+        
+        if timerLabel.text == "00:00" {
+            timer.invalidate()
+            timerRunning = false
+            timerCount = defaulttimer
+        }
+    }
+    
+    
+    
+    
+    func startTimer(){
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("timerCountDown"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func startButton(sender: AnyObject) {
+        if timerRunning == false{
+            startTimer()
+            timerRunning = true
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,8 +58,8 @@ class GameViewController: UIViewController {
         if let scene = GameScene(fileNamed:"GameScene") {
             // Configure the view.
             let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+            skView.showsFPS = false
+            skView.showsNodeCount = false
             
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
@@ -53,3 +94,4 @@ class GameViewController: UIViewController {
     
     
 }
+
